319d7299354db6d4fef2d8cc690bcf123178454d
Austin Stewart <astewart9841@Kailees-MacBook-Air.local>
2024-11-06 11:07:28 -0500

    Updated login functionality

--- API/universalData.py
+++ API/universalData.py
@@ -2,19 +2,13 @@ import pymssql
 import os
 
 def create_connection():
-    try:
-        # Establish connection using Cloud SQL Unix socket path
-        connection = pymssql.connect(
-            host="/cloudsql/sports-predictions-440800:us-east5:projections-database",
-            user="sqlserver",              # Database username
-            password="Yubullyme69!",        # Database password
-            database="predictionsDatabase"  # Target database name
-        )
-        print("Connection successful!")  # Debug: confirms successful connection
-        return connection
-    except pymssql.OperationalError as e:
-        print("Error connecting to the database:", e)  # Debugging message for connection issues
-        raise
+        pymssql.connect(
+        server=os.getenv("DB_SERVER"),
+        port=int(os.getenv("DB_PORT", 1433)),  # Default to 1433
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
 
 
 current_year = 2024
