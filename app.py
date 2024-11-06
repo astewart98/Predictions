133bf3e1b622f6071e3cacfd8d85d47ca817afee
Austin Stewart <astewart9841@Kailees-MacBook-Air.local>
2024-11-06 03:49:03 -0500

    Updated login functionality

--- app.py
+++ app.py
@@ -548,4 +548,4 @@ def save_matchup_stats():
             return jsonify({'message': 'Matchup stats saved successfully!'}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
