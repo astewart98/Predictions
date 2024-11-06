2bac904d4e01f1b644c18b89c8f22f01f63c6c20
Austin Stewart <astewart9841@Kailees-MacBook-Air.local>
2024-11-06 12:42:42 -0500

    Updated login functionality

--- API/universalData.py
+++ API/universalData.py
@@ -7,7 +7,7 @@ load_dotenv()
 class DatabaseConnection:
     def __init__(self):
         # Initialize the connection
-        connection_name = "sports-predictions-440800:us-east5:projections-database"
+        connection_name = "sports-predictions-440800:projections-database"
         self.connection = pymssql.connect(
             server=connection_name,
             port=int(os.getenv("DB_PORT", 1433)),  # Default to 1433
