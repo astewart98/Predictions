73c07b281d80b24380cb683e1c00dc4d7f57f62b
Austin Stewart <astewart9841@Kailees-MacBook-Air.local>
2024-11-06 13:14:04 -0500

    Updated login functionality

--- API/universalData.py
+++ API/universalData.py
@@ -6,23 +6,13 @@ load_dotenv()
 
 class DatabaseConnection:
     def __init__(self):
-        # Determine the environment and connection string
-        db_user = os.getenv("DB_USER")
-        db_password = os.getenv("DB_PASSWORD")
-        db_name = os.getenv("DB_NAME")
-
-        # Get the Cloud SQL connection name from environment variable
-        cloud_sql_connection_name = os.getenv("CLOUDSQL_CONNECTION_NAME")
-        
-        # Cloud SQL Unix socket file path
-        unix_socket = f"/cloudsql/{cloud_sql_connection_name}"
-
         # Initialize the connection
         self.connection = pymssql.connect(
-            server=unix_socket,
-            user=db_user,
-            password=db_password,
-            database=db_name
+            server=os.getenv("DB_SERVER"),
+            port=int(os.getenv("DB_PORT", 1433)),  # Default to 1433
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
 
     def __enter__(self):
