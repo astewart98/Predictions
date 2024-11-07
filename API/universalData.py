8d52c92ff206e8122c731567d357eb4e14422d36
Austin Stewart <astewart9841@Kailees-MacBook-Air.local>
2024-11-07 05:07:37 -0500

    Updated login functionality

--- API/universalData.py
+++ API/universalData.py
@@ -6,21 +6,18 @@ load_dotenv()
 
 class DatabaseConnection:
     def __init__(self):
-        # Initialize the connection
         self.connection = pymssql.connect(
             server=os.getenv("DB_SERVER"),
-            port=int(os.getenv("DB_PORT", 1433)),  # Default to 1433
+            port=int(os.getenv("DB_PORT", 1433)),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
 
     def __enter__(self):
-        # Return the connection object when entering the `with` block
         return self.connection
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # Close the connection when exiting the `with` block
         self.connection.close()
 
 def create_connection():
